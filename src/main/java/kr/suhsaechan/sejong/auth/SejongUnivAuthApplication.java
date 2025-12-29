bee73d0094c139f6869a6040101327f008425b1b
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-12-29 18:47:57 +0900

    프로젝트 기본 골격 구현 및 Spring 초기화 필요 : feat : 프로젝트 기본 코드 추가 https://github.com/Cassiiopeia/SUH-sejong-univ-auth/issues/1

---
+++ src/main/java/kr/suhsaechan/sejong/auth/SejongUnivAuthApplication.java
@@ -0,0 +1,14 @@
+package kr.suhsaechan.sejong.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejongUnivAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SejongUnivAuthApplication.class, args);
+	}
+
+}
+
